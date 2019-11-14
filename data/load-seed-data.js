90aa694a7793c5868ba224665418343d2936d9b8
Travis Molter <travismolter@gmail.com>
2019-11-14 10:25:00 -0800

    Pushing to heroku

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,7 @@
 const client = require('../lib/client');
 // import our seed data:
 const todos = require('./todos');
+const users = require('./users');
 
 run();
 
@@ -9,13 +10,28 @@ async function run() {
     try {
         await client.connect();
 
+        const savedUser = await Promise.all(
+            users.map(async user => {
+                const result = await client.query(`
+                INSERT INTO users (email, hash, display_name)
+                VALUES ($1, $2, $3)
+                RETURNING *;
+                `,
+                [user]);
+                return result.row[0];
+            })
+        );
+
         await Promise.all(
             todos.map(todo => {
+                const user = savedUser.find(user => {
+                    return user.id === todo.users;
+                });
                 return client.query(`
-                    INSERT INTO todos (task, complete)
-                    VALUES ($1, $2);
+                    INSERT INTO todos (task, complete, users_id)
+                    VALUES ($1, $2, $3);
                 `,
-                [todo.task, todo.complete]);
+                [todo.task, todo.complete, user.id]);
             })
         );
 
